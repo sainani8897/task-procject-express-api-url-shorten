5d54139024b2b9c9e4e7104023fcb3159983fb07
sainani8897 <sainani8897@gitlab.com>
2022-04-13 22:47:23 +0530

    works on users mongodb

--- src/server.js
+++ src/server.js
@@ -1,52 +1,84 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ObjectId } = require("mongodb");
 const port = 3000;
-
-const shortUrl = require("node-url-shortener");
-var TinyURL = require("tinyurl");
+const shortid = require("shortid");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+/** Mongo Client */
+const client = new MongoClient("mongodb://localhost:27017/test_crud");
+
+const collection = client.db().collection("short_urls");
+
+const baseUrl = `http://localhost:${port}/`;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/", (req, res) => {
-  // res.send("Hello World!");
-  console.log(req.body);
-  if (req.body) {
-    const data = req.body;
-    if (data.url && validURL(data.url)) {
-      //   shortUrl.short(data.url, function (err, url) {
-      //     if (err) {
-      //         res.send(err);
-      //     }
-      //     res.send({
-      //         status:200,
-      //         data:{
-      //             url
-      //         }
-      //     })
-      //   });
+app.get("/:code", async (req, res) => {
+  try {
+    const url = await collection.findOne({
+      code: req.params.code,
+    });
+    if (url) {
+      return res.redirect(url.original_url);
+    } else {
+      return res.status(404).json("No URL Found");
+    }
+  } catch (err) {
+    // exception handler
+    console.error(err);
+    res.status(500).json("Server Error");
+  }
+});
 
-      TinyURL.shorten(data.url, function (url, err) {
-        if (err) {
-          res.send(err);
-        }
-        res.send({
+app.post("/", async (req, res) => {
+  try {
+    if (req.body) {
+      const data = req.body;
+
+      const url = await collection.findOne({
+        original_url: data.url,
+      });
+
+      if(url){
+        return res.send({
           status: 200,
           data: {
-            url,
+            url: url.short_url,
           },
         });
-      });
-    } else {
-      return res.send({
-        error: "give a valid url",
-      });
+      }
+
+
+      if (data.url && validURL(data.url)) {
+        const code = shortid.generate();
+        const short_url = baseUrl + code;
+        const response = await collection.insertOne({
+          short_url,
+          code,
+          original_url: data.url,
+        });
+
+        return res.send({
+          status: 200,
+          data: {
+            url: short_url,
+          },
+        });
+      } else {
+        return res.send({
+          error: "give a valid url",
+        });
+      }
     }
+  } catch (error) {
+    return res.status(500).send({error});
   }
+  
 });
 
 function validURL(str) {
@@ -62,6 +94,13 @@ function validURL(str) {
   return !!pattern.test(str);
 }
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
-});
+client
+  .connect()
+  .then(function (client) {
+    app.listen(port, () => {
+      console.log("Server Started at Port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
